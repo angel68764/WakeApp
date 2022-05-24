7e059b26f9144a39ad7e019c7ce57d41a1c177a6
angel <angelandress687@gmail.com>
2022-05-25 01:07:01 +0200

    Navigation bar with activities

--- app/src/main/java/com/google/codelabs/mdc/java/wakeapp/AlarmActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/wakeapp/AlarmActivity.java
@@ -2,9 +2,12 @@ package com.google.codelabs.mdc.java.wakeapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,16 +16,42 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.util.ArrayList;
 
 public class AlarmActivity extends AppCompatActivity {
     ArrayList<Alarm> alarms = new ArrayList<>();
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-        Button button = (Button) findViewById(R.id.buttonPanel);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.alarmScreen);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.chronoScreen:
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),ChronoActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.alarmScreen:
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),AlarmActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
 
         RecyclerView rwAlarms = (RecyclerView) findViewById(R.id.recyclerAlarmList);
 
@@ -37,14 +66,15 @@ public class AlarmActivity extends AppCompatActivity {
 
         rwAlarms.setLayoutManager(new LinearLayoutManager(this));
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),ChronoActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+
+    /*private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //this is a helper class that replaces the container with the fragment. You can replace or add fragments.
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null); //if you add fragments it will be added to the backStack. If you replace the fragment it will add only the last fragment
+        transaction.commit(); // commit() performs the action
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/google/codelabs/mdc/java/wakeapp/ChronoActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/wakeapp/ChronoActivity.java
@@ -2,21 +2,50 @@ package com.google.codelabs.mdc.java.wakeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ChronoActivity extends AppCompatActivity {
     private Chronometer chronometer;
     private long pauseOffset;
     private boolean running;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chrono);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.chronoScreen);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.chronoScreen:
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),ChronoActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.alarmScreen:
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),AlarmActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
 
         chronometer = findViewById(R.id.chronometer);
         chronometer.setFormat("Time: %s");
